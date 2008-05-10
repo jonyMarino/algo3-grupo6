00d5d8d01c2c3ae0498f37d8a5f77f25fc258311
lucashpandolfo <lucashpandolfo@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-05-10 17:10:28 +0000

    Hay un tema con el mezclador. Miren los comentarios.

--- Car Tunning Experience/Tp/auto/partesAuto/mezclador/Mezclador.java
+++ Car Tunning Experience/Tp/auto/partesAuto/mezclador/Mezclador.java
@@ -19,6 +19,14 @@ public Mezclador(int eficiencia,TanqueNafta tanqueNafta) {
 public double obtenerMezcla(double litrosDeMezcla){
 		TanqueNafta tanqueNaftaAux = this.getTanqueNafta();
 		Nafta naftaAux = tanqueNaftaAux.getTipoNafta();
+		// Creo yo que no es la forma mas conveniente de hacerlo. ¿Que pasa si le pedimos al tanque que
+		// use mas nafta de la que tiene? Creo que debería hacerse al revés. Propongo algo así:
+		
+		// tanqueNafta.usarNafta(litrosDeMezcla);
+		// double mezcla = litros+(litros *(200 - (this.getEficiencia() + naftaAux.getOctanaje())));
+		
+		// Creo que sería "mas adecuado"
+		
 		double mezcla = litrosDeMezcla+(litrosDeMezcla *(200 - (this.getEficiencia() + naftaAux.getOctanaje())));
 		tanqueNafta.usarNafta(mezcla);
 		return mezcla;
@@ -30,7 +38,11 @@ public int getEficiencia(){
 }
 
 public void setEficiencia(int eficiencia) {
-	this.eficiencia = eficiencia;
+	if(eficiencia < 0)
+	   this.eficiencia = 0;
+	else if (eficiencia > 100)
+		this.eficiencia = 100;
+	else this.eficiencia=eficiencia;
 }
 
 public TanqueNafta getTanqueNafta() {
---
+++ Car Tunning Experience/Tp/auto/partesAuto/mezclador/MezcladorNafta.java
@@ -0,0 +1,12 @@
+package auto.partesAuto.mezclador;
+
+import auto.partesAuto.tanque.TanqueNafta;
+
+public class MezcladorNafta extends Mezclador {
+
+	public MezcladorNafta(int eficiencia, TanqueNafta tanqueNafta) {
+		super(eficiencia, tanqueNafta);
+		// TODO Auto-generated constructor stub
+	}
+
+}
