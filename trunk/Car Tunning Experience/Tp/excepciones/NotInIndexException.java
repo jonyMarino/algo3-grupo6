65f42bcb7d71ef27cd5075edcd773c6d1d66d811
vicky1986 <vicky1986@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-05-28 16:45:57 +0000

    Creacion del paquete excepciones para mejor organizacion.

---
+++ trunk/Car Tunning Experience/Tp/excepciones/NotInIndexException.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+public class NotInIndexException extends Exception{
+	static final long serialVersionUID=10;
+	
+	public NotInIndexException(String string){
+		super(string);
+	}
+}
