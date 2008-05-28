fc226af05983ca10edb6c4223e2baaf42c0e359f
vicky1986 <vicky1986@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-05-28 04:01:47 +0000

    falta todavia, pero por ahora esta lo que se necesita para el test taller.

---
+++ Car Tunning Experience/Tp/programaAuto/ProgramaAuto.java
@@ -0,0 +1,50 @@
+import auto.*;
+import auto.partesAuto.*;
+import auto.partesAuto.caja.*;
+import auto.partesAuto.tanque.*;
+import auto.partesAuto.mezclador.*;
+import combustible.Nafta;
+import pista.*;
+
+//TODO:esta en proceso todavia, se que falta completar, pero por ahora esta lo que
+//se necesita para el test taller.
+public class ProgramaAuto {
+	
+	private Auto auto;
+	private Pista pista;
+	
+	public ProgramaAuto () {
+		this.auto=null;
+		this.pista=null;	
+	}
+		
+	public Auto autoInicial(){
+		Nafta nafta = new Nafta(85,15);
+		TanqueNafta tanque = new TanqueNafta(70, nafta);
+		Carroceria carroceria = new Carroceria(5,5,250);
+		Rueda rueda = new Rueda(1,0.7,0.4);
+		MezcladorNafta mezclador = new MezcladorNafta(100,tanque);
+		Escape escape = new Escape(100);
+		Motor motor=new Motor(100,5000,mezclador,escape,2.0);
+		CajaManual caja = new CajaManual();
+		Auto auto = new AutoManual(escape,carroceria,motor,caja,mezclador,tanque,rueda,rueda,rueda,rueda);
+		return auto;
+	}
+	
+	public void setAuto(Auto auto){
+		this.auto = auto;
+	}
+
+	public Auto getAuto(){
+		return this.auto;
+	}
+
+	public void setPista(Pista pista){
+		this.pista = pista;
+	}
+
+	public Pista getPista(){
+		return this.pista;
+	}
+
+}
