47e5ae7032538ba7e268afbbbf3a8b63290efd8c
lucashpandolfo <lucashpandolfo@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-05-29 17:53:02 +0000

    Mucho mas trabajo en Taller. Ahora verifica que no existan partes repetidas en el catalogo. Si alguna parte se repita, la agrupa.

--- trunk/Car Tunning Experience/Tp/taller/Taller.java
+++ trunk/Car Tunning Experience/Tp/taller/Taller.java
@@ -3,34 +3,42 @@ package taller;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Dictionary;
+import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Iterator;
 
 import excepciones.NotEnoughMoneyException;
 import excepciones.NotInIndexException;
+import excepciones.PartAlreadyInCatalogEsception;
 import excepciones.WrongPartClassException;
 
 import programaAuto.Usuario;
 
 import auto.Auto;
 import auto.ParteAuto;
+import auto.partesAuto.BoundsException;
 import auto.partesAuto.Carroceria;
 import auto.partesAuto.Eje;
 import auto.partesAuto.Escape;
 import auto.partesAuto.Motor;
 import auto.partesAuto.caja.Caja;
 import auto.partesAuto.mezclador.Mezclador;
+import auto.partesAuto.tanque.TanqueCombustible;
 
 public class Taller {
 
 	private int ultimoNumeroDeParteEnElCatalogo;
 	private class InformacionParteAutoEnElTaller{
 		int precio;
+		int cantidad; //Número de partes con estas características
 		ParteAuto parte;
-		
-		public InformacionParteAutoEnElTaller(ParteAuto parte, int precioEnAlgo$){
+		private String descripcion;
+				
+		public InformacionParteAutoEnElTaller(ParteAuto parte, int precioEnAlgo$, String descripcion){
 			precio = precioEnAlgo$;
 			this.parte = parte;
+			this.descripcion = descripcion;
+			cantidad = 1;
 		}
 		
 		public Class getTipoDeParte(){
@@ -44,6 +52,24 @@ public class Taller {
 		public ParteAuto getParte(){
 			return parte;
 		}
+		
+		public String getDescripcion(){
+			return descripcion;
+		}
+		
+		public int getCantidad(){
+			return cantidad;
+		}
+		
+		public int incrementarCantidad(){
+			return ++cantidad;
+		}
+		
+		public int decrementarCantidad() throws BoundsException{
+			if (cantidad<=0)
+				throw new BoundsException("No hay mas partes con esas características disponibles");
+			return --cantidad;
+		}
 	}
 	
 	private ArrayList<Integer> listaDePartes;
@@ -55,13 +81,34 @@ public class Taller {
 		hashDePartes = new Hashtable<String, InformacionParteAutoEnElTaller>();
 	}
 	
-	public int catalogar(ParteAuto parte, int precioEnAlgo$) {
-		ultimoNumeroDeParteEnElCatalogo++;
-		listaDePartes.add(new Integer(ultimoNumeroDeParteEnElCatalogo));
-		String clave = new String("PARTE_"+ ultimoNumeroDeParteEnElCatalogo);
-		InformacionParteAutoEnElTaller informacion =  new InformacionParteAutoEnElTaller(parte, precioEnAlgo$);
-		hashDePartes.put(clave, informacion);
-		return ultimoNumeroDeParteEnElCatalogo;
+	public int catalogar(ParteAuto parte, int precioEnAlgo$, String descripcion) {
+		try{
+			buscarParteRepetida(parte, precioEnAlgo$, descripcion);
+			ultimoNumeroDeParteEnElCatalogo++;
+			listaDePartes.add(new Integer(ultimoNumeroDeParteEnElCatalogo));
+			String clave = new String("PARTE_"+ ultimoNumeroDeParteEnElCatalogo);
+			InformacionParteAutoEnElTaller informacion =  new InformacionParteAutoEnElTaller(parte, precioEnAlgo$, descripcion);
+			hashDePartes.put(clave, informacion);
+			return ultimoNumeroDeParteEnElCatalogo;
+		}catch (PartAlreadyInCatalogEsception miExcepcion){
+			InformacionParteAutoEnElTaller informacion = hashDePartes.get(miExcepcion.getMessage());
+			informacion.incrementarCantidad();
+			return Integer.parseInt((miExcepcion.getMessage().substring("PARTE_".length() )));
+			//Si, es una línea MUY fea
+			//despues veo si la hago mas legible
+		}
+	}
+
+	private void buscarParteRepetida(ParteAuto parte, int precioEnAlgo$, String descripcion) throws PartAlreadyInCatalogEsception {
+		Enumeration misClaves = hashDePartes.keys();
+		InformacionParteAutoEnElTaller miParteInfo = null;
+		String unaClave = null;
+		while(misClaves.hasMoreElements()){
+			unaClave = (String) misClaves.nextElement();
+			miParteInfo = hashDePartes.get(unaClave);
+			if ( (miParteInfo.getDescripcion() == descripcion) && (miParteInfo.getPrecio() == precioEnAlgo$) )
+				throw new PartAlreadyInCatalogEsception(unaClave);
+		}
 	}
 
 	public void comprar(Usuario usuario, int indiceDelCatalogo) throws NotEnoughMoneyException,NotInIndexException, WrongPartClassException{
@@ -70,10 +117,8 @@ public class Taller {
 			throw new NotEnoughMoneyException("El usuario no posee dinero suficiente para comprar esta parte.");
 		else{
 			usuario.gastarDinero(informacionDeEstaParte.getPrecio());
-			//usuario.ensamblarParte(informacionDeEstaParte.getParte());
 			ensamblarParteAuto(usuario.getAuto(), informacionDeEstaParte.getParte());
-			eliminarParteAutoEnElTaller(indiceDelCatalogo);
-			
+			venderParteAutoDelTaller(indiceDelCatalogo);
 		}
 	}
 
@@ -94,42 +139,53 @@ public class Taller {
 		
 	}
 	
-	private void eliminarParteAutoEnElTaller(Integer indiceEnElCatalogo) throws NotInIndexException{
+	private void venderParteAutoDelTaller(Integer indiceEnElCatalogo) throws NotInIndexException{
 		if(! (hashDePartes.containsKey("PARTE_"+indiceEnElCatalogo)) )
 			throw new NotInIndexException("No existe ese número de parte en el catalogo.");
-		else hashDePartes.remove("PARTE_"+indiceEnElCatalogo);
+		else {
+			try {
+				hashDePartes.get("PARTE_"+indiceEnElCatalogo).decrementarCantidad();
+			} catch (BoundsException unaExcepcion) {
+				hashDePartes.remove("PARTE_"+indiceEnElCatalogo);
+			}
+		}
 	}
 	
 	public ParteAuto ensamblarParteAuto(Auto unAuto, ParteAuto unaParte) throws WrongPartClassException{
-		ParteAuto parteTemporal = unaParte;
-		
-		if(unaParte instanceof Escape){
+		if(unaParte instanceof Escape)
 			return ensamblarEscape(unAuto, (Escape) unaParte);
-		}
-		else if(unaParte instanceof Carroceria){
+		else if(unaParte instanceof Carroceria)
 			return ensamblarCarroceria(unAuto, (Carroceria) unaParte);
-		}
-		else if(unaParte instanceof Caja){
+		else if(unaParte instanceof Caja)
 			return ensamblarCaja(unAuto, (Caja)unaParte);
-		}
-		else if(unaParte instanceof Motor){
+		else if(unaParte instanceof Motor)
 			return ensamblarMotor(unAuto, (Motor)unaParte);
-		}
-		else if( unaParte instanceof Eje){
+		else if( unaParte instanceof Eje)
 			return ensamblarEje(unAuto, (Eje)unaParte);
-		}
-		else if( unaParte instanceof Mezclador){
+		else if( unaParte instanceof Mezclador)
 			return ensamblarMezclador(unAuto, (Mezclador)unaParte);
-		}
-		
-		return parteTemporal;
+		else if( unaParte instanceof TanqueCombustible)
+			return ensamblarTanqueCombustible(unAuto, (TanqueCombustible)unaParte);
+		else return unaParte;
 	}
 	
+	private ParteAuto ensamblarTanqueCombustible(Auto unAuto, TanqueCombustible unTanque) throws WrongPartClassException {
+		TanqueCombustible parteTemporal = unAuto.getTanqueCombustible();
+		if( ! (unTanque.getClass() == parteTemporal.getClass()) ){
+			throw new WrongPartClassException("No se puede ensamblar la parte. Se requiere de " + parteTemporal.getClass().getSimpleName() + " pero se tiene " + unTanque.getClass().getSimpleName());
+		}
+		unAuto.setTanqueCombustible(unTanque);
+		return parteTemporal;
+	}
+
 	private ParteAuto ensamblarMezclador(Auto unAuto, Mezclador unMezclador) throws WrongPartClassException {
 		Mezclador parteTemporal = unAuto.getMezclador();
 		if( ! (unMezclador.getClass() == parteTemporal.getClass()) ){
-			throw new WrongPartClassException("No se puede ensamblar la parte. Se requiere de " + parteTemporal.getClass() + " pero se tiene " + unMezclador.getClass());
+			throw new WrongPartClassException("No se puede ensamblar la parte. Se requiere de " + parteTemporal.getClass().getSimpleName() + " pero se tiene " + unMezclador.getClass().getSimpleName());
 		}
+		unMezclador.setTanqueCombustible(unAuto.getTanqueCombustible());
+		parteTemporal.setTanqueCombustible(null);
+		unAuto.setMezclador(unMezclador);
 		return parteTemporal;
 	}
 
@@ -158,7 +214,7 @@ public class Taller {
 		unaCaja.setEje(unAuto.getEje());
 		unaCaja.setMotor(unAuto.getMotor());
 		parteTemporal.setMotor(null);
-		parteTemporal.setEje(null);
+		//parteTemporal.setEje(null);
 		unAuto.setCaja(unaCaja);
 		return parteTemporal;
 	}
@@ -173,4 +229,5 @@ public class Taller {
 		unAuto.setMotor(unMotor);
 		return parteTemporal;
 	}
+
 }
