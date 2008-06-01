8847564b6a533eb5ec9d520787e6ed1d06cfb896
lucashpandolfo <lucashpandolfo@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-06-01 18:34:49 +0000

    Intento 1 de implementar lo expresado en el diagrama (todavia falta...)

--- trunk/Car Tunning Experience/Tp/programaAuto/Usuario.java
+++ trunk/Car Tunning Experience/Tp/programaAuto/Usuario.java
@@ -3,19 +3,18 @@ import java.util.ArrayList;
 
 import auto.Auto;
 import auto.ParteAuto;
+import auto.partesAuto.BoundsException;
 import excepciones.NotEnoughMoneyException;
 
 public class Usuario {
 	private double dinero;
 	private Auto auto;
-	private ArrayList<ParteAuto> partesDisponibles;
 	private String nombre;
 	
 	public Usuario(String nombre, Auto miAuto) {
 		this.nombre = nombre;
 		setAuto(miAuto);
 		setDinero(1000);
-		partesDisponibles = new ArrayList<ParteAuto>();
 	}
 	private void setAuto(Auto miAuto) {
 		this.auto = miAuto;
@@ -33,6 +32,13 @@ public class Usuario {
 		if(cantidadDeDineroAGastar > getDinero())
 			throw new NotEnoughMoneyException("El usuario no puede gastar mas dinero del que tiene.");
 		else setDinero(getDinero()-cantidadDeDineroAGastar);
+	}
+	
+	public void adquirirDinero(double cantidadDeDineroAdquirido) throws BoundsException{
+		if(cantidadDeDineroAdquirido < 0){
+			throw new BoundsException("No se puede adquirir unacantidad Negativa de dinero. Use gastarDinero(:double);.");
+		}
+		else setDinero(getDinero()+cantidadDeDineroAdquirido);
 	}
 	
 }
