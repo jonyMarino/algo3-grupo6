c34c25a040288ffc838eb17bcfa416a4497de7a1
vicky1986 <vicky1986@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-06-22 22:20:42 +0000

    Antes no se subio bien ControladorTaller.

---
+++ trunk/Car Tunning Experience/Tp/controlador/ControladorTaller.java
@@ -0,0 +1,23 @@
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import taller.Taller;
+import vista.PanelBase;
+
+public class ControladorTaller implements ActionListener {
+	
+	private PanelBase panel;
+	private Taller taller;
+	
+	public ControladorTaller() {
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
