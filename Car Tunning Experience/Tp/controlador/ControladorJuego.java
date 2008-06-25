93fabdee362ee0a847b231c538c09a9dec06fd21
Vero13 <Vero13@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-06-25 21:37:08 +0000

    Subo modificacaciones controladores, en un rato lo adapto a la modificion de Marino.

--- Car Tunning Experience/Tp/controlador/ControladorJuego.java
+++ Car Tunning Experience/Tp/controlador/ControladorJuego.java
@@ -2,7 +2,16 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Iterator;
+
+import javax.swing.ImageIcon;
+
 import excepciones.WrongUserNameException;
+import programaAuto.NoPistaPickedException;
+import programaAuto.NotAbleWhileSimulatingException;
+import programaAuto.NotContainedPistaException;
+import programaAuto.Pista;
+import programaAuto.PistaPickedException;
 import programaAuto.ProgramaAuto;
 import vista.Boton;
 import vista.PanelBase;
@@ -40,13 +49,25 @@ public class ControladorJuego implements ActionListener {
 	private void inicializarJuego(){
 			try {
 				programaAuto = new ProgramaAuto(((PantallaUsuario)panelBase.getPantallaActual()).obtenerNombreBoxUsuario());
-				this.controladorTaller = new ControladorTaller(programaAuto, ((PantallaUsuario)panelBase.getPantallaActual()).obtenerNombreBoxUsuario(), ((PantallaUsuario)panelBase.getPantallaActual()).obtenerImagenSeleccionada());
+						
+				try {
+					programaAuto.setPista(this.generarProximaPista());
+				} catch (NotContainedPistaException e1) {} 
+				  catch (PistaPickedException e1) {}
+				
+				try {
+					this.controladorTaller = new ControladorTaller(programaAuto.entrarAlTaller());
+				} catch (NoPistaPickedException e) {}
+				  catch (NotAbleWhileSimulatingException e) {}
+				
+				ImageIcon imagen = ((PantallaUsuario)panelBase.getPantallaActual()).obtenerImagenSeleccionada(); 
 				
 				panelBase.crearPantalla(new PantallaTaller(this));	
-				this.controladorTaller.setPantallaTaller((PantallaTaller)panelBase.getPantallaActual());
+				controladorTaller.setPantallaTaller( (PantallaTaller)panelBase.getPantallaActual() );
 				
 				controladorTaller.setProximaPista(programaAuto.getPista());
-				controladorTaller.actualizarPantallaTaller();
+				controladorTaller.cargarPantallaTaller(imagen);	
+				
 			} catch (WrongUserNameException e) {
 				this.MensajeError();
 			}
@@ -59,6 +80,17 @@ public class ControladorJuego implements ActionListener {
 
 	public void setPanelBase(PanelBase panelBase) {
 		this.panelBase = panelBase;
+	}
+	
+	public void entrarATaller(){
+		
+	}
+	
+	//TODO: provisorio para que entre al taller, esto lo debe generar el programa auto
+	private Pista generarProximaPista(){
+		Iterator<Pista> pistas = programaAuto.getPistas();
+		
+		return (pistas.next());
 	}
 
 }
--- Car Tunning Experience/Tp/controlador/ControladorTaller.java
+++ Car Tunning Experience/Tp/controlador/ControladorTaller.java
@@ -1,10 +1,16 @@
 package controlador;
 
+import java.util.Enumeration;
+import java.util.Hashtable;
+
 import javax.swing.ImageIcon;
 
+
+
+import excepciones.BoundsException;
 import programaAuto.Pista;
-import programaAuto.ProgramaAuto;
 import programaAuto.Taller;
+import proveedorDePartes.fabricas.ParteAuto;
 import vista.PantallaTaller;
 
 public class ControladorTaller {
@@ -12,34 +18,32 @@ public class ControladorTaller {
 	private Taller taller;
 	private PantallaTaller pantallaTaller;
 	private Pista proximaPista;
-	private ImageIcon avatarUsuario;
-	private String nombreUsuario;
 	
-	public ControladorTaller(ProgramaAuto programaAuto, String nombreUsuario , ImageIcon avatarUsuario) {
-		this.avatarUsuario = avatarUsuario;
-		this.nombreUsuario = nombreUsuario;
+	public ControladorTaller(Taller taller) {
+		this.taller = taller;
 	}
 
-	public void actualizarPantallaTaller() {
+	public void cargarPantallaTaller(ImageIcon avatarUsuario) {
 		//PANEL DINERO
-		pantallaTaller.actualizarInformacionDinero( "$$$" );
+		pantallaTaller.actualizarInformacionDinero( Double.toString(taller.getUsuario().getDinero()) );
 		
 		//PANEL PROXIMA PISTA
-		//pantallaTaller.actualizarInformacionPista( proximaPista );
+		pantallaTaller.actualizarInformacionPista( Double.toString(proximaPista.getLongitud()) , proximaPista.getNombre(), Integer.toString(proximaPista.getVelocidadAire()) );
 		
 		//PANEL RESERVA
-		//pantallaTaller.actualizarInformacionReserva( taller );
+		this.actualizarInformacionReserva();
 			
 		//PANEL NAFTA
-		//pantallaTaller.actualizarInformacionNafta( taller.getUsuario().getAuto() );
+		pantallaTaller.actualizarInformacionNafta( taller.getUsuario().getAuto().getTanqueCombustible().getCantidadCombustible() , taller.getUsuario().getAuto().getTanqueCombustible().getCapacidad() );
 		
 		//PANEL USUARIO
-		pantallaTaller.actualizarInformacionUsuario( nombreUsuario, avatarUsuario );
+		pantallaTaller.actualizarInformacionUsuario( taller.getUsuario().getNombre(), avatarUsuario );
 		
 		//PANEL AUTO
-		pantallaTaller.actualizarInformacionAuto( taller.getUsuario().getAuto() );
-	
+		this.actualizarInformacionAuto();
 	}
+
+
 
 	public void setPantallaTaller(PantallaTaller pantallaTaller) {
 		this.pantallaTaller = pantallaTaller;
@@ -48,5 +52,43 @@ public class ControladorTaller {
 	public void setProximaPista(Pista proximaPista) {
 		this.proximaPista = proximaPista;
 	}
+
+	private void actualizarInformacionReserva() {	
+		pantallaTaller.limpiarInformacionReserva();
+		
+		String descripcion,vidaUtil;
+		boolean cargo = false;
+		
+		while (taller.getPartesDeReserva().hasNext()){
+			cargo = true;
+			try {
+				descripcion = taller.getPartesDeReserva().next().getInformacionModelo().getCaracteristica("DESCRIPCION");
+				vidaUtil = Double.toString(taller.getPartesDeReserva().next().getVidaUtil());
+				pantallaTaller.agregarAReserva(descripcion, vidaUtil);
+			} catch (BoundsException e) {}	    	
+		}
+		
+		if(!cargo){
+			pantallaTaller.agregarAReserva("- Lista ", "Vacía -");
+		}
+	}
 	
+	private void actualizarInformacionAuto() {
+		pantallaTaller.limpiarInformacionAuto();
+		
+		ParteAuto parte;
+		Hashtable<String,ParteAuto> tabla=new Hashtable<String,ParteAuto>();
+        tabla = taller.getUsuario().getAuto().getHashDePartes();
+        Enumeration<ParteAuto> enumeracion = tabla.elements();
+        String vidaUtil,nombreParte;
+         
+        while(enumeracion.hasMoreElements()){   
+      	   		parte=enumeracion.nextElement();
+      	   		try{
+                   nombreParte = parte.getInformacionDelModelo().getCaracteristica("DESCRIPCION");
+                   vidaUtil = Double.toString(parte.getVidaUtil());
+                   pantallaTaller.agregarInformacionAuto(nombreParte, vidaUtil);
+      	   		}catch (BoundsException e){}
+         }
+	}
 }
