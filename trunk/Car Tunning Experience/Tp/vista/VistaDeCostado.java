f22deef34a257d58c097737e874bfefd6883a307
lucashpandolfo <lucashpandolfo@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-06-26 20:54:46 +0000

    Mas trabajo en la vista de la carrera.

--- trunk/Car Tunning Experience/Tp/vista/VistaDeCostado.java
+++ trunk/Car Tunning Experience/Tp/vista/VistaDeCostado.java
@@ -5,6 +5,7 @@ import java.awt.Frame;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -18,6 +19,7 @@ public class VistaDeCostado implements VistaGrafica {
 	Image unArbol;
 	Image unArbolNevado;
 	ProgramaAuto elPrograma;
+	ArrayList<Integer> listaDeArboles;
 	
 	public VistaDeCostado(int ancho, int alto, ProgramaAuto unPrograma){
 		bufferPrincipal = new BufferedImage(ancho,alto, BufferedImage.TYPE_INT_RGB);
@@ -31,15 +33,24 @@ public class VistaDeCostado implements VistaGrafica {
 		elPrograma = unPrograma;
 		
 		autoPrincipal = new ImageIcon(getClass().getResource("/vista/images/"+ "UnAuto" +".gif")).getImage();
-		//unArbol = new ImageIcon(getClass().getResource("/vista/images/"+ "arbolito" +".gif")).getImage();
-		//unArbolNevado = new ImageIcon(getClass().getResource("/vista/images/"+ "arbolitonevado" +".gif")).getImage();
+		unArbol = new ImageIcon(getClass().getResource("/vista/images/"+ "arbolito" +".gif")).getImage();
+		unArbolNevado = new ImageIcon(getClass().getResource("/vista/images/"+ "arbolitonieve" +".gif")).getImage();
 		
 		generarVista();
 
 	}
 
 	private void generarVista() {
-		///elPrograma.getPista().getLongitud()
+		int cantidadArboles = 5;
+		int distanciaEntreArboles = (int) (elPrograma.getPista().getLongitud()/cantidadArboles);
+		
+		System.out.println(distanciaEntreArboles);
+		
+		listaDeArboles = new ArrayList<Integer>();
+		
+		for(int i=0; i<cantidadArboles; i++){
+			listaDeArboles.add(i*distanciaEntreArboles);
+		}
 	}
 
 	public int getHeight() {
@@ -51,10 +62,33 @@ public class VistaDeCostado implements VistaGrafica {
 		BufferedImage temporal = new  BufferedImage(getWidth(),getHeight(), BufferedImage.TYPE_INT_RGB);
 		Graphics2D temporal2D = (Graphics2D) temporal.getGraphics();
 		temporal2D.drawImage(bufferPrincipal,0, 0, null);
-		temporal2D.drawImage(autoPrincipal,(int) elPrograma.getUsuario().getAuto().getPosicion(), getHeight()*3/4-autoPrincipal.getHeight(null)*7/8, null);
+		int inicio=0;
+		int posicion =  (int) elPrograma.getUsuario().getAuto().getPosicion();
+		int anchoAuto = autoPrincipal.getWidth(null);
+		if(posicion > (getWidth()-anchoAuto)/2){
+			inicio = posicion-getWidth()/2;
+			posicion = (getWidth()-anchoAuto)/2;
+		}
+		dibujarArboles(temporal2D, inicio);
+		temporal2D.drawImage(autoPrincipal,posicion, getHeight()*3/4-autoPrincipal.getHeight(null)*7/8, null);
 		return temporal;
 	}
 
+
+	private void dibujarArboles(Graphics2D temporal, int posicionDeInicio) {
+		System.out.println(posicionDeInicio);
+		System.out.println(unArbol.getWidth(null));
+		for( Integer p:listaDeArboles){
+			if((p>=(posicionDeInicio-unArbol.getWidth(null))) && (p<=(posicionDeInicio+getWidth()))){
+				temporal.drawImage(unArbol, p-posicionDeInicio, getHeight()*3/4-unArbol.getHeight(null)+5, unArbol.getWidth(null), unArbol.getHeight(null), null);
+				//
+			}
+			else{
+
+			}
+		}
+		
+	}
 
 	public int getWidth() {
 		return bufferPrincipal.getWidth();
