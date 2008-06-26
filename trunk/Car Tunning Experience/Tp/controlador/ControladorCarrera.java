d388b49d8f47f17a3fd13bee7ce8fcab12a2335e
lucashpandolfo <lucashpandolfo@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-06-26 15:26:03 +0000

    Bosquejo de la carrera.

---
+++ trunk/Car Tunning Experience/Tp/controlador/ControladorCarrera.java
@@ -0,0 +1,127 @@
+package controlador;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Observable;
+import java.util.Observer;
+
+import auto.Auto;
+
+import programaAuto.NoPistaPickedException;
+import programaAuto.NotAbleWhileSimulatingException;
+import programaAuto.NotInTallerException;
+import programaAuto.NotSimulatingException;
+import programaAuto.ProgramaAuto;
+import proveedorDePartes.fabricas.CajaManual;
+import vista.PantallaCarrera;
+import excepciones.BoundsException;
+
+public class ControladorCarrera implements KeyListener, Observer{
+
+	ProgramaAuto programaAuto;
+	Auto unAuto;
+	PantallaCarrera laPantalla;
+	boolean simulando;
+	double tiempo;
+	
+	public ControladorCarrera(ProgramaAuto programaAuto, PantallaCarrera pantalla) {
+		this.programaAuto = programaAuto;
+		unAuto = programaAuto.getUsuario().getAuto();
+		laPantalla=pantalla;
+		simulando = true;
+		tiempo = 10;
+	}
+
+	public void keyPressed(KeyEvent e) {
+		double aceleracion=0;
+		switch (e.getKeyChar()) {
+		case '0': aceleracion=0; break;
+		case '1': aceleracion=0.01; break;
+		case '2': aceleracion=0.05; break;
+		case '3': aceleracion=0.1; break;
+		case '4': aceleracion=0.5; break;
+		case '5': aceleracion=1; break;
+		default: aceleracion=-1;
+			break;
+		}
+		try {
+			unAuto.presionarAcelerador(aceleracion);
+			unAuto.setPosicion(unAuto.getPosicion()+aceleracion*10);
+			laPantalla.setXAuto((int)unAuto.getPosicion());
+			laPantalla.repaint();
+		} catch (BoundsException e1) {
+		}
+	}
+
+
+	public void keyReleased(KeyEvent e) {
+		double aceleracion=0;
+		switch (e.getKeyChar()) {
+			case '1': aceleracion=0; 
+			case '2': aceleracion=0;
+			case '3': aceleracion=0;
+			case '4': aceleracion=0; 
+			case '5': aceleracion=0;
+			try {
+					unAuto.presionarAcelerador(0);
+			} catch (BoundsException e1) {} break;
+		default: break;
+		}
+	}
+
+
+	public void keyTyped(KeyEvent e) {
+		if(e.getKeyChar() == 'c'){
+			int cambioActual=unAuto.getCaja().getCambio();
+			((CajaManual)unAuto.getCaja()).setCambioManual(cambioActual+1);
+		}
+		if(e.getKeyChar() == 'v'){
+			int cambioActual=unAuto.getCaja().getCambio();
+			((CajaManual)unAuto.getCaja()).setCambioManual(cambioActual-1);
+		}
+		if(e.getKeyChar() == 'q'){
+			simulando=false;
+		}
+	}
+	
+	public void update(Observable o, Object arg) {
+		if (o==programaAuto)
+		{
+			laPantalla.setXAuto((int)unAuto.getPosicion());
+		}
+	}
+
+	public void comenzar() {
+
+		try {
+			programaAuto.entrarAlTaller();
+			programaAuto.entrarALaCarrera();
+		} catch (NoPistaPickedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (NotInTallerException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (NotAbleWhileSimulatingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		
+		while (simulando){
+		unAuto.simular(tiempo*100);
+			try {
+			Thread.sleep((int)(tiempo));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	}
+
+	/*try {
+		programaAuto.correr();
+	} catch (NotSimulatingException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}	*/
+	}
+}
