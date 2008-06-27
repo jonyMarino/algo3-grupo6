46e3c6b4781aa8978a68bb1186cf7d167dab0a24
lucashpandolfo <lucashpandolfo@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-06-27 02:31:33 +0000

    Mas trabajo en la vista de la carrera.

--- Car Tunning Experience/Tp/vista/PantallaCarrera.java
+++ Car Tunning Experience/Tp/vista/PantallaCarrera.java
@@ -6,8 +6,11 @@ import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Point;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
@@ -17,6 +20,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
+
+import programaAuto.ProgramaAuto;
 
 import sun.awt.RepaintArea;
 import sun.java2d.loops.DrawRect;
@@ -30,9 +35,10 @@ public class PantallaCarrera extends JPanel{
 
 
     ControladorCarrera elControlador;
-    int xAuto, yAuto;
     VistaGrafica unaVista;
-    
+    ProgramaAuto elPrograma;
+	ArrayList<Point2D> listaDeArboles;
+	
 	public PantallaCarrera(ControladorJuego controladorJuego) {
 		super();
 		int width = 900;
@@ -40,14 +46,36 @@ public class PantallaCarrera extends JPanel{
 		this.setSize(width, height);
 		this.setBorder(BorderFactory.createEmptyBorder(2, 0, 2,0));
 		this.add(new JSeparator(JSeparator.VERTICAL),BorderLayout.LINE_START);
-		
+		elPrograma = controladorJuego.getProgramaAuto();
+		elControlador = new ControladorCarrera(elPrograma, this);
+		generarArboles();
 		unaVista = new VistaDeCostado(width, height, controladorJuego.getProgramaAuto());
+		((VistaDeCostado)unaVista).setArboles(listaDeArboles);
 		setFocusable(true);
 		setVisible(true);
-		elControlador = new ControladorCarrera(controladorJuego.getProgramaAuto(), this);
 		addKeyListener(elControlador);
 		elControlador.comenzar();
 	}
+	
+	private void generarArboles() {
+		int cantidadArboles = 5;
+		int distanciaEntreArboles = (int) (elPrograma.getPista().getLongitud()/cantidadArboles);
+		
+		System.out.println(distanciaEntreArboles);
+		
+		listaDeArboles = new ArrayList<Point2D>();
+		
+		for(int i=0; i<cantidadArboles; i++){
+			int y=0;
+			do{
+				y = (int) Math.abs((Math.random()*100))&5;
+			}while(y==2);
+			System.out.println(y);
+			Point2D temporal = new Point((int) (i*distanciaEntreArboles),y);
+			listaDeArboles.add(temporal);
+		}
+	}
+
 	
 	public void paintComponent(Graphics g) {
 		 Graphics2D g2 =(Graphics2D) g;
@@ -56,19 +84,4 @@ public class PantallaCarrera extends JPanel{
 	        }
 	}
 
-	public int getXAuto() {
-		return xAuto;
-	}
-
-	public void setXAuto(int auto) {
-		xAuto = auto;
-	}
-
-	public int getYAuto() {
-		return yAuto;
-	}
-
-	public void setYAuto(int auto) {
-		yAuto = auto;
-	}
 }
