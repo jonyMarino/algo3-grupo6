7ebb9f458679cec33182652a6d52d6faf2ffa3cc
vicky1986 <vicky1986@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-06-29 23:59:14 +0000

    Falta arreglar detalles no esta terminado.

---
+++ trunk/Car Tunning Experience/Tp/proveedorDeNafta/ProveedorDeNafta.java
@@ -0,0 +1,46 @@
+package proveedorDeNafta;
+
+import java.util.ArrayList;
+import programaAuto.Usuario;
+import proveedorDeNafta.FabricaDeNafta;
+import proveedorDePartes.fabricas.InformacionDelModelo;
+import excepciones.BoundsException;
+import excepciones.NotEnoughMoneyException;
+import excepciones.TankIsFullException;
+
+public class ProveedorDeNafta {
+	
+	private ArrayList<FabricaDeNafta> fabricasDisponibles;
+	
+	public ProveedorDeNafta(){
+		fabricasDisponibles = new ArrayList<FabricaDeNafta> ();
+		agregarFabrica(new FabricaDeNafta());	
+	}
+	
+	public double comprar(double cantidad,Usuario usuario) throws NotEnoughMoneyException, BoundsException, TankIsFullException{
+		InformacionDelModelo modelo =getFabricasDisponibles().get(0).getTipos().get(0);
+		//como siempre usamos el mismo tipo de nafta por eso esto es interno
+		double dineroNecesario = 0;
+		try {
+			dineroNecesario = Double.parseDouble(modelo.getCaracteristica("COSTO"))*cantidad;
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BoundsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+			usuario.gastarDinero(dineroNecesario);
+			return cantidad;
+	}
+	
+	public ArrayList<FabricaDeNafta> getFabricasDisponibles(){
+		return fabricasDisponibles;
+	}
+	
+	public void agregarFabrica(FabricaDeNafta unaFabrica) {
+		fabricasDisponibles.add(unaFabrica);	
+	}
+
+}
