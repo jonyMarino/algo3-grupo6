698f1dabe73c22e4976cd66904c34a8c4b21914f
vicky1986 <vicky1986@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-07-02 01:43:32 +0000

    Como el pedal acelerador no tiene posibilidad de ser cambiado, ahora no se desgasta. Igual hay que borrarlo.

--- Car Tunning Experience/Tp/proveedorDePartes/fabricas/Pedal.java
+++ Car Tunning Experience/Tp/proveedorDePartes/fabricas/Pedal.java
@@ -1,53 +1,40 @@
 package proveedorDePartes.fabricas;
+
 import excepciones.BoundsException;
 import excepciones.PartBrokenException;
 
 /**
+ * 
  * El Pedal encapsula el comportamiento y las características de un pedal.
  *
  * @see PartesAuto
+ * 
  */
 public abstract class Pedal extends ParteAuto {
 
 	private boolean usado;
 
-	/**
-	 * Crea un nuevo pedal.
-	 */
 	Pedal() {
 		this.setUsado(false);
 	}
 
-	/**
-	* Realiza la presion sobre el pedal.
-	*
-	* @param intensidad La intensidad con que se presiona.
-	*
-	* @throws BoundsException
-	*/
 	public abstract void presionar(double intensidad) throws PartBrokenException;
 
-	/**
-	* Produce el desgaste del pedal debido al uso y al tiempo.
-	*
-	* @param tiempo El tiempo que se desgasta.
-	*/
 	public void desgastar(double tiempo) {
-		try{
-			if(getVidaUtil()!=0){
-				setVidaUtil(getVidaUtil()-tiempo/100);
-			}
-		}
-		catch(BoundsException e){
+		if(isUsado()){
 			try{
-				setVidaUtil(0);
-			}catch(BoundsException f){}
+				if(getVidaUtil()!=0){
+					setVidaUtil(getVidaUtil()-tiempo/100);
+				}
+			}
+			catch(BoundsException e){
+				try{
+					setVidaUtil(0);
+				}catch(BoundsException f){}
+			}
 		}
 	}
 
-	/**
-	 * Devuelve true si el pedal ha sido utilizado.
-	 */
 	public boolean isUsado() {
 		return usado;
 	}
