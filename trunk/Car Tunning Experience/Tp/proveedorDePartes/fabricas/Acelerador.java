149e60e305e7fb08283003be5500d35e63435d4e
vicky1986 <vicky1986@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-07-02 01:43:32 +0000

    Como el pedal acelerador no tiene posibilidad de ser cambiado, ahora no se desgasta. Igual hay que borrarlo.

--- trunk/Car Tunning Experience/Tp/proveedorDePartes/fabricas/Acelerador.java
+++ trunk/Car Tunning Experience/Tp/proveedorDePartes/fabricas/Acelerador.java
@@ -1,61 +1,37 @@
 package proveedorDePartes.fabricas;
 
 import excepciones.BoundsException;
-import excepciones.PartBrokenException;
 
 /**
+ * 
  * El {@link Pedal} del Acelerador.
  *
  * @see Pedal
+ * 
  */
+
 public class Acelerador extends Pedal {
 
 	private	Motor motor;
 
-	/**
-	 * Crea un nuevo pedal Acelerador con las características especificadas.
-	 *
-	 * @param motor El {@link Motor}.
-	 *
-	 * @see Motor
-	 */
 	Acelerador(Motor motor) {
 		super();
 		this.setMotor(motor);
 	}
 
-	/**
-	* Realiza la presion sobre el Acelerador.
-	*
-	* @param intensidad La intensidad con que se presiona.
-	*/
-	public void presionar(double intensidad) throws PartBrokenException {
+	public void presionar(double intensidad) {
 		try {
-			super.setUsado(intensidad > 0);
-			getMotor().acelerar(intensidad);
+			if (intensidad > 0)
+				getMotor().acelerar(intensidad);
 		} catch (BoundsException e) {
 			e.printStackTrace();
 		} 
 	}
 
-	/**
-	 * Devuelve el {@link Motor} asociado al Acelerador.
-	 *
-	 * @return El {@link Motor} asociado.
-	 *
-	 * @see Motor
-	 */
 	public Motor getMotor() {
 		return motor;
 	}
 
-	/**
-	 * Asigna un {@link Motor} al Acelerador.
-	 *
-	 * @param motor El {@link Motor} a asignar.
-	 *
-	 * @see Motor
-	 */
 	public void setMotor(Motor motor) {
 		this.motor = motor;
 	}
--- trunk/Car Tunning Experience/Tp/proveedorDePartes/fabricas/Freno.java
+++ trunk/Car Tunning Experience/Tp/proveedorDePartes/fabricas/Freno.java
@@ -1,24 +1,20 @@
 package proveedorDePartes.fabricas;
-import excepciones.BoundsException;
+
 import excepciones.PartBrokenException;
 
 /**
- * El {@link Pedal} del Freno.
+ * 
+ * El pedal Freno es el encargado de generar un fuerza contraria a la aceleracion del auto.
  *
  * @see Pedal
+ * 
  */
 public class Freno extends Pedal implements Torqueador {
 
 	private double torque;
 	private double pastillaDeFreno;
 	private Eje eje;
-	/**
-	 * Crea un nuevo pedal Freno con las características especificadas.
-	 *
-	 * @param pastillaDeFreno La pastilla de freno.
-	 *
-	 * @see Eje
-	 */
+
 	Freno(double pastillaDeFreno) {
 		super();
 		this.setTorque(0);
@@ -33,26 +29,14 @@ public class Freno extends Pedal implements Torqueador {
 			eje.addTorqueador(this);		
 	}
 
-	/**
-	* Realiza la presion sobre el Freno.
-	*
-	* @param intensidad La intensidad con que se presiona.
-	*
-	* @throws BoundsException
-	*/
 	public void presionar(double intensidad) throws PartBrokenException {
-		super.setUsado(false);
+		setUsado(false);
 		if (this.getVidaUtil() > 0){
 			this.setTorque(- (intensidad * this.getPastillaDeFreno()));
 		} else
 			throw new PartBrokenException("El Freno se rompio");
 	}
 
-	/**
-	 * Devuelve el Torque asociado al Freno.
-	 *
-	 * @see Torque
-	 */
 	public double getTorque() {
 		return torque;
 	}
@@ -61,20 +45,10 @@ public class Freno extends Pedal implements Torqueador {
 		this.torque = intensidad;
 	}
 
-	/**
-	 * Devuelve la pastilla de freno asociada al Freno.
-	 *
-	 * @return Pastilla de freno asociada.
-	 */
 	public double getPastillaDeFreno() {
 		return pastillaDeFreno;
 	}
 
-	/**
-	 * Le asigna una pastilla de freno al Freno.
-	 *
-	 * @param pastillaDeFreno La pastilla de freno a asignar.
-	 */
 	public void setPastillaDeFreno(double pastillaDeFreno) {
 		this.pastillaDeFreno = pastillaDeFreno;
 	}
