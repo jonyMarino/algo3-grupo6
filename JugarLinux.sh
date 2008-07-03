bc68de8652408fd1f867b0a6f09d9e6edcbe4b12
lucashpandolfo <lucashpandolfo@1e71df3f-ab4c-0410-bda5-471e0501c314>
2008-07-03 17:41:34 +0000

    Helpers para jugar (Linux y Windows).

---
+++ JugarLinux.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+java -cp .:xom-1.1.jar CarTunningExperience
+reset
+echo "Gracias por jugar!!!!!"
---
+++ JugarWindows.bat
@@ -0,0 +1,5 @@
+@echo off
+java -cp .;xom-1.1.jar CarTunningExperience
+@echo on
+cls
+echo "Gracias por jugar!!!!"
